ae4306eb2500aa996d757ed97e1bf38dd204628d
Lava0moon <anushasaxena7sa@gmail.com>
2025-03-22 00:47:00 -0400

    fixed frontend issues

--- wallet-shield/src/App.jsx
+++ wallet-shield/src/App.jsx
@@ -1,19 +1,19 @@
-
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import GetStarted from "./pages/GetStarted";
 import "./styles.css";
+
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/getstarted" element={<GetStarted />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- wallet-shield/src/pages/Home.jsx
+++ wallet-shield/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import HeroSection from "../components/HeroSection";
+import HeroSection from "../components/HeroSection"
+
 const Home = () => {
   return (
     <div>
