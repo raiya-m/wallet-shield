0ba2b62ff0bcb76eed04268e4ba49c3798dfb1ab
Lava0moon <anushasaxena7sa@gmail.com>
2025-03-21 23:57:03 -0400

    imports backend

--- backend/app.py
+++ backend/app.py
@@ -1,2 +1,9 @@
-#hello 
-#set up backend
+from flask import Flask, request, jsonify, redirect, url_for
+from flask_cors import CORS
+import json
+
+
+import numpy
+from sklearn.ensemble import IsolationForest
+app = Flask(__name__)
+CORS(app)  
