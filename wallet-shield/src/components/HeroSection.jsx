c6cac4a033f10ec2d4a84a38f74e40e7f05f54e6
Lava0moon <anushasaxena7sa@gmail.com>
2025-03-21 23:38:29 -0400

    hero section

---
+++ wallet-shield/src/components/HeroSection.jsx
@@ -0,0 +1,36 @@
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GetStarted from '../pages/GetStarted';
+import { Link } from "react-router-dom";
+import "../styles.css";
+import logo from "../assets/logo.png.png";
+import { useNavigate } from 'react-router-dom';
+
+const HeroSection = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/getstarted'); 
+  };
+  
+    return (
+        <div className="hero">
+          <h1>WalletShield</h1>
+          <p>Empowering Students to Secure Their Crypto Transactions</p>
+          <div className="hero-container">
+                <p1>AI Driven Analysis. Blockchain Integration.</p1>
+
+                <p2>Leverage AI-powered fraud detection and blockchain transparency to stay protected, learn smarter, and navigate crypto confidently. Gain educational insights powered by Google Gemini.</p2>
+                <button className="hero-button" onClick={handleClick}>Analyze my Wallet</button>
+
+            </div>
+            <img src={logo} alt="Wallet Image" className="hero-image" />
+          
+            
+        </div>
+        
+      );
+};
+
+ 
+export default HeroSection;
