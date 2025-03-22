ee982361efeb898439bb3a3235f5dd46a32de7e3
Raiya Minhas <raiya.minhas@gmail.com>
2025-03-22 15:52:06 -0400

    imported (hidden) key

--- wallet-shield/src/App.jsx
+++ wallet-shield/src/App.jsx
@@ -4,19 +4,20 @@ import Navbar from "./components/Navbar";
 import CryptoChatbot from './components/CryptoChatbot';
 import Home from "./pages/Home";
 import GetStarted from "./pages/GetStarted";
-import "./styles.css";
 import Aiananalysis from "./pages/Aiananalysis";
- 
+import "./styles.css";
 
 function App() {
   return (
     <>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/getstarted" element={<GetStarted />} />
         <Route path="/aianalysis" element={<Aiananalysis />} />
       </Routes>
+      <CryptoChatbot />
     </>
   );
 }
