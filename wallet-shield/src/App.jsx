72f2f572114d415575cf0385091647a96aef54cc
Raiya Minhas <raiya.minhas@gmail.com>
2025-03-22 15:21:27 -0400

    chatbot frontend

--- wallet-shield/src/App.jsx
+++ wallet-shield/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import CryptoChatbot from './components/CryptoChatbot';
 import Home from "./pages/Home";
 import GetStarted from "./pages/GetStarted";
 import "./styles.css";
