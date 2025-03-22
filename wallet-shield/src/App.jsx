a220b113658bb5c62be6f13fce493e9566f6a53e
Lava0moon <anushasaxena7sa@gmail.com>
2025-03-22 00:20:09 -0400

    frontendn issue fix

--- wallet-shield/src/App.jsx
+++ wallet-shield/src/App.jsx
@@ -5,8 +5,6 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import GetStarted from "./pages/GetStarted";
 import "./styles.css";
-import AIAnalysis from './pages/Aianalysis';
-
 function App() {
   return (
     <Router>
@@ -14,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/getstarted" element={<GetStarted />} />
-        <Route path="/aianalysis" element={<AIAnalysis />} />
       </Routes>
     </Router>
   );
